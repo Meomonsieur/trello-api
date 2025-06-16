e9a07f1452bb796899b20c0ce81f28c0e24590c4
Meomonsieur <phandangquan0212@gmail.com>
2025-06-17 00:47:03 +0700

    Init Code

---
+++ src/server.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+import express from 'express';
+
+const app = express()
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', function(req, res) {
+    res.send('<h1>Hello World Nodejs</h1>')
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Port running server at http://${hostname}:${port}/`)
+})
