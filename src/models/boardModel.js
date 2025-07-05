987c6211244e837e42419c26856546bb129c24ee
Meomonsieur <phandangquan0212@gmail.com>
2025-07-05 22:58:35 +0700

    aggregate

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -3,6 +3,10 @@ import Joi from 'joi'
 import { ObjectId } from 'mongodb'
 import { GET_DB } from '~/config/mongodb'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
+import {BOARD_TYPES} from '~/utils/constants'
+import { columnModel } from '~/models/columnModel'
+import { cardModel } from '~/models/cardModel'
+
 
 // define collection(Name & Schema)
 
@@ -12,6 +16,7 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   title: Joi.string().required().min(3).max(50).trim().strict(),
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
+  type: Joi.string().valid(BOARD_TYPES.PUBLIC, BOARD_TYPES.PRIVATE).required(),
   
   columnOrderIds: Joi.array().items(
     Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
@@ -55,9 +60,27 @@ const findOneById = async (id) => {
 const getDetails = async (id) => {
   try {
 
-    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-      _id: new ObjectId(id)
-    })
+    //const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({ _id: new ObjectId(id) })
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).aggregate([
+      { $match: { 
+        _id: new ObjectId(id),
+        _destroy: false
+      
+      } },
+      { $lookup: {
+        from: columnModel.COLUMN_COLLECTION_NAME,
+        localField: '_id',
+        foreignField: 'boardId',
+        as: 'columns'
+      } },
+      { $lookup: {
+        from: cardModel.CARD_COLLECTION_NAME,
+        localField: '_id',
+        foreignField: 'boardId',
+        as: 'cards'
+      } }
+    ]).toArray()
+    
     return result
   } catch (error) {
     throw new Error(error)
@@ -71,3 +94,7 @@ export const boardModel = {
   findOneById,
   getDetails
 }
+
+// boardID: 6868f6b72c95a57bf2caf7f8
+//column ID 6868fe48b203023bff4ed498
+// card ID: 686900425acab03b4ab6d305
